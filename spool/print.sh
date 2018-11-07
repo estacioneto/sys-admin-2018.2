25c8f4aee7aa981bcd97d6fea4d0c001aeb2108a
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-11-07 18:00:52 -0300

    Add initial authorization of quota limit

--- spool/print.sh
+++ spool/print.sh
@@ -2,6 +2,17 @@
 
 LP_SCRIPT=/usr/bin/lp.orig
 
-# TODO: implement authentication
+quota=`cat /var/lib/spool/quota`
+current_user=`whoami`
+report=`cat /var/lib/spool/current_report`
+
+current_quota=`echo "$report" | awk -v user=$current_user '$0 ~ user { print $2 }'`
+
+if [ $current_quota -ge $quota ]; then
+  echo "[!] You've exceeded you quota limit for this month"
+  exit 1
+fi
+
+# TODO: update quota
 
 $LP_SCRIPT "$@"
