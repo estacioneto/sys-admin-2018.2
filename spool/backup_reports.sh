831688bdd7c5513c905ef055721c3759edb6ab03
Estácio Pereira <estacio.pereira@gmail.com>
2018-11-07 17:41:18 -0300

    Report script

--- spool/backup_reports.sh
+++ spool/backup_reports.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-backup_filepath="/var/lib/spool/backup/$(date +%m-%Y)"
+backup_filepath="/var/lib/spool/backup/report-$(date +%m-%Y).txt"
 current_report_path="/var/lib/spool/current_report"
 
 # TODO: Quota definition logic
--- spool/report.sh
+++ spool/report.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+report_path=/var/lib/spool/current_report
+
+if [ -z $1 ]; then
+  report_path=/var/lib/spool/current_report
+elif [ ! -z $1 ]; then
+  mes=$1
+  ano=`date +%Y`
+  if [ ! -z $2 ]; then
+    ano=$2
+  fi
+
+  report_path=/var/lib/spool/backups/report-$mes-$ano.txt
+fi
+
+printf "%-20s  | %s\n" Usuário Cota
+printf "=%.0s" {1..40}
+printf "\n"
+awk '{ printf "%-20s | %d\n", $1, $2 }' $report_path
+
