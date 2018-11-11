c73207a8f61ee6fa37f941a4765fc3a3420218da
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-11-11 20:07:12 -0300

    Fix awk command for backup

--- spool/backup_reports.sh
+++ spool/backup_reports.sh
@@ -4,7 +4,7 @@ backup_filepath="/var/lib/spool/backup/report-$(date +%m-%Y).txt"
 current_report_path="/var/lib/spool/current_report"
 
 quota=`cat /var/lib/spool/quota`
-new_report=`awk -v quota=$quota'{ print $1 ($2 + quota) }' $current_report_path`
+new_report=`awk -v quota=$quota '{ new_quota = $2 - quota; print $1, (new_quota < 0) ? 0 : new_quota; }' $current_report_path`
 
 mv $current_report_path $backup_filepath && echo -e "$new_report" > $current_report_path && chmod -w $backup_filepath
 
